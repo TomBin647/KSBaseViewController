98a8e9faaabaf10abc8d4898c8202ca5cade9afc
Howard <bing.hao1982@gmail.com>
2015-09-14 15:50:47 +0800

    Initial Commit

---
+++ KSBaseViewController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KSBaseViewController
+//
+//  Created by bing.hao on 15/9/14.
+//  Copyright (c) 2015年 Bing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
